ddc18d62e3b0b284ba8c2773a14e279d16718d66
Glowstoner <glowstoner1@gmail.com>
2018-11-08 23:22:14 +0100

    Update ArmorContents.java
    Optimisation de la méthode #equip(Player p) en passant par la fin des nom d'items grâce à la méthode String#endWith(String suffix). Il est bon à retenir que les enums en Java possèdent certaines méthodes comme Enum#name() pour avoir le nom de l'enum sous forme de String. Les enums héritent de toutes les méthodes de la classe Object, naturellement.

--- src/be/goldocelot/lg/utils/ArmorContents.java
+++ src/be/goldocelot/lg/utils/ArmorContents.java
@@ -1,13 +1,12 @@
 package be.goldocelot.lg.utils;
 
-
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 /**
  * 
- * @author Nicolas Gerard (Goldocelot)
- * Class utilitaire qui permet d'équiper plus facilement un item à un joueur
+ * @author Nicolas Gerard (Goldocelot), Glowstoner
+ * Class utilitaire qui permet d'Ã©quiper plus facilement un item Ã  un joueur
  */
 public class ArmorContents {
 	
@@ -17,93 +16,21 @@ public class ArmorContents {
 		this.item = item;
 	}
 	
-	/* Méthode qui vérifie la nature d'un item (casque, plastron, jambière, botte ou item neutre) 
+	/* MÃ©thode qui vÃ©rifie la nature d'un item (casque, plastron, jambiÃ¨re, botte ou item neutre) 
 	et qui le place dans le bon slot du joueur en fonction de celle-ci */
 	public void equip(Player p) {
-		switch(item.getType()) {
-		case CHAINMAIL_BOOTS:{
-			p.getEquipment().setBoots(this.item);
-		}
-			break;
-		case CHAINMAIL_CHESTPLATE:{
-			p.getEquipment().setChestplate(this.item);
-		}
-			break;
-		case CHAINMAIL_HELMET:{
-			p.getEquipment().setHelmet(this.item);
-		}
-			break;
-		case CHAINMAIL_LEGGINGS:{
-			p.getEquipment().setLeggings(this.item);
-		}
-			break;
-		case DIAMOND_BOOTS:{
-			p.getEquipment().setBoots(this.item);
-		}
-			break;
-		case DIAMOND_CHESTPLATE:{
-			p.getEquipment().setChestplate(this.item);
-		}
-			break;
-		case DIAMOND_HELMET:{
-			p.getEquipment().setHelmet(this.item);
-		}
-			break;
-		case DIAMOND_LEGGINGS:{
-			p.getEquipment().setLeggings(this.item);
-		}
-			break;
-		case GOLD_BOOTS:{
-			p.getEquipment().setBoots(this.item);
-		}
-			break;
-		case GOLD_CHESTPLATE:{
-			p.getEquipment().setChestplate(this.item);
-		}
-			break;
-		case GOLD_HELMET:{
-			p.getEquipment().setHelmet(this.item);
-		}
-			break;
-		case GOLD_LEGGINGS:{
-			p.getEquipment().setLeggings(this.item);
-		}
-			break;
-		case IRON_BOOTS:{
-			p.getEquipment().setBoots(this.item);
-		}
-			break;
-		case IRON_CHESTPLATE:
-			break;
-		case IRON_HELMET:{
-			p.getEquipment().setHelmet(this.item);
-		}
-			break;
-		case IRON_LEGGINGS:{
-			p.getEquipment().setLeggings(this.item);
-		}
-			break;
-		case LEATHER_BOOTS:{
-			p.getEquipment().setBoots(this.item);
-		}
-			break;
-		case LEATHER_CHESTPLATE:{
-			p.getEquipment().setChestplate(this.item);
-		}
-			break;
-		case LEATHER_HELMET:{
-			p.getEquipment().setHelmet(this.item);
-		}
-			break;
-		case LEATHER_LEGGINGS:{
-			p.getEquipment().setLeggings(this.item);
-		}
-			break;
-		default:{
-			p.getInventory().addItem(this.item);
-		}
-			break;
+		String mn = this.item.getType().name();
 		
+		if(mn.endsWith("HELMET")) {
+			p.getEquipment().setHelmet(this.item);
+		}else if(mn.endsWith("CHESTPLATE")) {
+			p.getEquipment().setChestplate(this.item);
+		}else if(mn.endsWith("LEGGINGS")) {
+			p.getEquipment().setLeggings(this.item);
+		}else if(mn.endsWith("BOOTS")) {
+			p.getEquipment().setLeggings(this.item);
+		}else {
+			p.getInventory().addItem(this.item);
 		}
 	}
 }
